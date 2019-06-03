88df82484cb00956a886649ab839a8f06a5aedd0
Ryan Crawford <ryancrawfordny@gmail.com>
2019-06-03 19:50:32 -0400

    Added react

--- rails_bigfoot_api--api/app/controllers/comments_controller.rb
+++ rails_bigfoot_api--api/app/controllers/comments_controller.rb
@@ -14,4 +14,20 @@ class CommentsController < ApplicationController
          render json: { message: "there was some other error" }, status: :internal_server_error
        end
     end
+    def create
+        comment = Comment.find params[:comment_id]
+        Comment.comments << Comment.new(comment_params)
+        render json: { comment: comment.comments.last }
+      end
+   
+      def update
+        comment = Comment.where(comment_id: params[:comment_id], id: params[:id])
+        comment.update post_params
+        render json: { comment: comment }
+      end
+   
+       def destroy
+        comment = Comment.where(comment_id: params[:comment_id], id: params[:id])
+        comment.destroy(params[:id])
+      end
 end
