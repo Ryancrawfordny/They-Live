1adf8c9f1abb0aeed0bd914b57418a30331d8722
Ryan Crawford <ryancrawfordny@gmail.com>
2019-06-03 14:13:09 -0400

    commit

---
+++ rails_bigfoot_api--api/db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 2019_06_03_180059) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
